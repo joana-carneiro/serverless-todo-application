01791af7b3df5d50620b4503b7669d31f82accd7
joana <joanailc@gmail.com>
2020-05-15 17:32:48 +0100

    UPDATE TODO

---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,47 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS from "aws-sdk";
+import {parseUserId} from "../../auth/utils";
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+  const todoId = event.pathParameters.todoId
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  await docClient.update({
+    TableName: todosTable,
+    Key: {
+      todoId,
+      userId: parseUserId(jwtToken)
+    },
+    UpdateExpression: 'set #name = :n, #dueDate = :due, #done = :d',
+    ExpressionAttributeValues: {
+      ':n': updatedTodo.name,
+      ':due': updatedTodo.dueDate,
+      ':d': updatedTodo.done
+    },
+    ExpressionAttributeNames: {
+      '#name': 'name',
+      '#dueDate': 'dueDate',
+      '#done': 'done'
+    }
+  }).promise();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  }
+}
