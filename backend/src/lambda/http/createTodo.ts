e5cb581b8d0749b53ecd5db5e2aa7ee7960bf153
joana <joanailc@gmail.com>
2020-05-15 15:54:35 +0100

    Create TODO - with auto population of fields.

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -2,6 +2,8 @@ import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as uuid from 'uuid'
+import { parseUserId } from '../../auth/utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
@@ -10,12 +12,27 @@ const todosTable = process.env.TODOS_TABLE
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
   console.log('Processing event: ', event)
+  const itemId = uuid.v4()
 
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const item = {
+    todoId: itemId,
+    userId: parseUserId(jwtToken),
+    createdAt: new Date(Date.now()).toISOString(),
+    done: false,
+    ...newTodo
+  }
+
+
+
   await docClient.put({
     TableName: todosTable,
-    Item: newTodo
+    Item: item
   }).promise()
 
   return {
@@ -25,7 +42,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
-      newTodo
+      item
     })
   }
 }
