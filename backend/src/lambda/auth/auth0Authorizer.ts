f76489f7117979c72565efcb3b60b4cf3edaadf3
joana <joanailc@gmail.com>
2020-05-16 21:08:46 +0100

    Authentication and Authorization using certificate

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,29 +1,29 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-//import { verify, decode } from 'jsonwebtoken'
-import { verify} from 'jsonwebtoken'
+import { verify, decode } from 'jsonwebtoken'
+//import { verify} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-//import Axios from 'axios'
-//import { Jwt } from '../../auth/Jwt'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-const auth0Secret = process.env.AUTH_0_SECRET
+//const auth0Secret = process.env.AUTH_0_SECRET
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-//const jwksUrl = 'https://dev-0c7sbbrb.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-0c7sbbrb.auth0.com/pem'
+
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
+  console.log('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
+    console.log('User was authorized', jwtToken)
 
     return {
       principalId: jwtToken.sub,
@@ -60,9 +60,21 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
 
   const token = getToken(authHeader)
-  //const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  return verify(token, auth0Secret) as JwtPayload
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  if(!jwt){
+    throw new Error('invalid token')
+  }
+
+  try {
+    const response = await Axios.get(jwksUrl);
+    return verify(token,response.data,{algorithms:['RS256']}) as JwtPayload
+
+  } catch (error) {
+    console.error(error);
+    return undefined
+  }
 
 }
 
